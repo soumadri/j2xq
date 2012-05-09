85727ff2b25de41e7d885cfc1b93fe006b8664ed
soumadri <soumadri@gmail.com>
2012-05-09 16:48:39 +0530

    Added ANT build script and main batch script

--- j2xq/j2xq.bat
+++ j2xq/j2xq.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 echo Compiling interaces...
-javac -cp "j2xq.jar;marklogic-xcc-5.0.2.jar" %1
+javac -cp "dist/j2xq.jar;lib/marklogic-xcc-5.0.2.jar" %1
 echo Generating stubs and implementation classes...
-java -jar j2xq.jar
+java -jar dist/j2xq.jar
